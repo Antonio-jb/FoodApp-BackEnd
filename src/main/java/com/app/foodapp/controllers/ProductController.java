4101a712c1afee8f653b446ef432984a665f97ca
antonio-jb <antonio_ajb@outlook.com>
2025-03-03 23:21:47 +0100

    Pedidos

--- src/main/java/com/app/foodapp/controllers/ProductController.java
+++ src/main/java/com/app/foodapp/controllers/ProductController.java
@@ -16,7 +16,7 @@ import java.util.List;
 import java.util.Optional;
 
 @RestController
-@RequestMapping
+@RequestMapping("/products")
 public class ProductController {
 
     @Autowired
--- src/main/java/com/app/foodapp/services/ProductService.java
+++ src/main/java/com/app/foodapp/services/ProductService.java
@@ -3,10 +3,12 @@ package com.app.foodapp.services;
 import com.app.foodapp.models.Product;
 import com.app.foodapp.repositories.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
 
+@Service
 public class ProductService {
 
     @Autowired
